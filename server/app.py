ec2e2edc89c147965a8df1174ae157ca7bd4abc8
raditha dissanayake <raditha@raditha.com>
2024-07-28 09:47:10 +0530

    COrs needed

--- server/app.py
+++ server/app.py
@@ -1,17 +1,19 @@
 # app.py
-from flask import Flask, session, request, jsonify
-from connect import Connect4
 import pickle
-from dotenv import load_dotenv
 import os
+from flask import Flask, session, request, jsonify
+from flask_cors import CORS
+from dotenv import load_dotenv
+from connect import Connect4
 
 # Load environment variables from .env file
 load_dotenv()
 
 app = Flask(__name__)
 app.secret_key = os.getenv('SECRET_KEY')  # Load secret key from environment variable
+CORS(app, supports_credentials=True)
 
-@app.route('/start_game/', methods=['GET', 'POST'])
+@app.route('/start/', methods=['GET', 'POST'])
 def start_game():
     if request.method == 'POST':
         rows = request.json.get('rows', 6)
@@ -21,9 +23,9 @@ def start_game():
         cols = 7
     game = Connect4(rows, cols)
     session['game'] = pickle.dumps(game)
-    return jsonify({"message": "Game started", "rows": rows, "cols": cols})
+    return jsonify({"message": "Game started", "rows": rows, "cols": cols, "board": game.board})
 
-@app.route('/make_play/', methods=['POST'])
+@app.route('/move/', methods=['POST'])
 def make_play():
     if 'game' not in session:
         return jsonify({"error": "No game in progress"}), 400
