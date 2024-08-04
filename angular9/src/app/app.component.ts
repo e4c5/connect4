9af494b7a45660331aa09a12e07f69c6b59bc33e
raditha dissanayake <raditha@raditha.com>
2024-08-04 15:26:58 +0530

    So the game is now playable

--- angular9/src/app/app.component.ts
+++ angular9/src/app/app.component.ts
@@ -1,3 +1,4 @@
+// src/app/app.component.ts
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
@@ -8,13 +9,17 @@ import { HttpClient } from '@angular/common/http';
 })
 export class AppComponent {
   board: number[][] = [];
+  playing: boolean = true;
+  gameOverMessage: string = '';
 
   constructor(private http: HttpClient) {}
 
   startGame() {
-    this.http.get<{board: number[][], playing: boolean}>('http://localhost:5000/start/', { withCredentials: true })
+    this.http.get<{board: number[][], playing: boolean, message?: string}>('http://localhost:5000/start/', { withCredentials: true })
       .subscribe(response => {
         this.board = response.board;
+        this.playing = response.playing;
+        this.gameOverMessage = '';
         console.log('Start Game clicked', response);
       });
   }
@@ -24,9 +29,15 @@ export class AppComponent {
   }
 
   makeMove(column: number) {
-    this.http.post<{board: number[][], playing: boolean}>('http://localhost:5000/move/', { column }, { withCredentials: true })
+    if (!this.playing) return;
+
+    this.http.post<{board: number[][], playing: boolean, message?: string}>('http://localhost:5000/move/', { column }, { withCredentials: true })
       .subscribe(response => {
         this.board = response.board;
+        this.playing = response.playing;
+        if (!this.playing && response.message) {
+          this.gameOverMessage = response.message;
+        }
         console.log('Move made', response);
       });
   }
